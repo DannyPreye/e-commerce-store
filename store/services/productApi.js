1f91385970af4571c3f92069779bf447ead2c6f4
DannyPreye <99923947+DannyPreye@users.noreply.github.com>
2022-11-04 21:04:24 +0100

    Add test comment

--- store/services/productApi.js
+++ store/services/productApi.js
@@ -1,5 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { HYDRATE } from 'next-redux-wrapper';
+// tests
 
 export const productApi = createApi({
   reducerPath: 'productApi',
